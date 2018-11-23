22d449138d36e07a78737e2ae038fc6e163173f6
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-11-23 22:31:51 +0530

    add a watcher for css file change

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 const gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer');
 
+gulp.task('deafult', function () {
+	gulp.watch('css/**/*.css', ['styles']);
+});
+
 gulp.task('styles', function () {
 	gulp
 		.src('css/**/*.css')
