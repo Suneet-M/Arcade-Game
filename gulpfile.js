90405b4b9ce71fa9556582c1d62db130bbee39cc
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-11-26 13:31:56 +0530

    feat: Add gulp-concat plugin to unify all CSS and JS files
    New files named all.css and all.js
    This improves performance by reducing the number of http file requests sent to the github server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,58 @@
 /* eslint-env node */
 
+// reference gulp plugins
 const gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
-	browserSync = require('browser-sync').create();
+	browserSync = require('browser-sync').create(),
+	concat = require('gulp-concat');
 
 gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'],
 	function () {
 		gulp.watch('img/*', ['copy-images']);
 		gulp.watch('css/**/*.css', ['styles']);
-		gulp.watch('./index.html', ['copy-html']).on('change', browserSync.reload);
-		gulp.watch('js/**/*.js', ['scripts']).on('change', browserSync.reload);
+		gulp.watch('./index.html', ['copy-html'])
+			.on('change', browserSync.reload); // to reload site when HTML is edited
+		gulp.watch('js/**/*.js', ['scripts'])
+			.on('change', browserSync.reload); // to reload site when JS is edited
 
 		browserSync.init({
 			server: 'docs/'
-		});
+		}); // launch site with browsersync connected
 	}
 );
 
+// HTML
 gulp.task('copy-html', function() {
 	gulp
 		.src('./index.html')
 		.pipe(gulp.dest('docs/'));
 });
 
+// Images
 gulp.task('copy-images', function() {
 	gulp
 		.src('img/*')
 		.pipe(gulp.dest('docs/img/'));
 });
 
+// CSS
 gulp.task('styles', function () {
 	gulp
 		.src('css/**/*.css')
-		.pipe(autoprefixer({browsers: ['last 2 versions']}))
+		.pipe(autoprefixer({browsers: ['last 2 versions']})) // add vendor prefixes
+		.pipe(concat('all.css')) // unify all css files
 		.pipe(gulp.dest('docs/css/'))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream()); // stream changes to browsersync site
 });
 
+// JS
 gulp.task('scripts', function () {
 	gulp
-		.src('js/**/*.js')
+		.src([
+			'js/**/resources.js',
+			'js/**/engine.js',
+			'js/**/app.js',
+		]) // to preserve the sequence of code injected in index.html
+		.pipe(concat('all.js')) // unify all js files
 		.pipe(gulp.dest('docs/js/'));
 });
